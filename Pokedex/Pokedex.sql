7d9fcdb9000f5649d831c24a027d72c734948144
Jufa888 <157121650+Jufa888@users.noreply.github.com>
2024-10-24 12:32:01 +0200

    Añado la carpeta Pokedex

---
+++ Pokedex/Pokedex.sql
@@ -0,0 +1,61 @@
+insert into entrenador(Nombre,Genero,Rol) values
+("Chano","Masculino","Joven"),
+("Jaime","Masculino","Cazabichos"),
+("Rita","Femenino","Chica"),
+("Celia","Femenino","Dominguera"),
+("Gaspar","Masculino","Entrenador Guay"),
+("Evaristo","Masculino","Montañero"),
+("Ben","Masculino","Pokemaniaco"),
+("Lola","Femenino","Playera"),
+("Carla","Femenino","Modelo"),
+("Anselmo","Masculino","Anciano");
+-- -----------------------------------------------------------------------------------------------------------
+insert into generacion(Numero,Region,Legendario1,Legendario2) values
+(1,"Kanto","Mew","Mewtwo"),
+(2,"Johto","Ho-oh","Lugia"),
+(3,"Hoenn","Groudon","Kyogre"),
+(4,"Sinnoh","Dialga","Palkia"),
+(5,"Teselia","Zekrom","Reshiram"),
+(6,"Kalos","Yveltal","Xerneas"),
+(7,"Alola","Solgaleo","Lunala"),
+(8,"Galar","Zacian","Zamazenta"),
+(9,"Paldea","Miraidon","Koraidon");
+-- -----------------------------------------------------------------------------------------------------------
+insert into gimnasio(Nombre,Tipo,Ciudad,Generacion_Numero) values
+("Gimnasio de Ciudad Plateada","Roca","Ciudad Plateada",1),
+("Gimnasio de Ciudad Celeste","Agua","Ciudad Celeste",1),
+("Gimnasio de Ciudad Trigal","Normal","Ciudad Trigal",2),
+("Gimnasio de Ciudad Endrino","Dragón","Ciudad Endrino",2),
+("Gimnasio de Pueblo Lavacalda","Fuego","Pueblo Lavacalda",3),
+("Gimnasio de Ciudad Malvalona","Eléctrico","Ciudad Malvalona",3),
+("Gimnasio de Ciudad Rocavelo","Lucha","Ciudad Rocavelo",4),
+("Gimnasio de Ciudad Corazón","Fantasma","Ciudad Corazón",4),
+("Gimnasio de Ciudad Fayenza","Tierra","Ciudad Fayenza",5),
+("Gimnasio de Ciudad Loza","Volador","Ciudad Loza",5),
+("Gimnasio de Ciudad Romantis","Hada","Ciudad Romantis",6),
+("Gimnasio de Ciudad Fluxus","Psíquico","Ciudad Fluxus",6),
+("Gimnasio de Pueblo Auriga","Hielo","Pueblo Auriga",8),
+("Gimnasio de Pueblo Crampón","Siniestro","Pueblo Crampón",8),
+("Gimnasio de Pueblo Pirotín","Bicho","Pueblo Pirotín",9),
+("Gimnasio de Pueblo Altamía","Planta","Pueblo Altamía",9);
+-- -----------------------------------------------------------------------------------------------------------
+insert into lider(Nombre,Tipo,Ciudad,Gimnasio_Nombre) values
+("Brock","Roca","Ciudad Plateada","Gimnasio de Ciudad Plateada"),
+("Misty","Agua","Ciudad Celeste","Gimnasio de Ciudad Celeste"),
+("Blanca","Normal","Ciudad Trigal","Gimnasio de Ciudad Trigal"),
+("Débora","Dragón","Ciudad Endrino","Gimnasio de Ciudad Endrino"),
+("Candela","Fuego","Pueblo Lavacalda","Gimnasio de Pueblo Lavacalda"),
+("Erico","Eléctrico","Ciudad Malvalona","Gimnasio de Ciudad Malvalona"),
+("Brega","Lucha","Ciudad Rocavelo","Gimnasio de Ciudad Rocavelo"),
+("Fantina","Fantasma","Ciudad Corazón","Gimnasio de Ciudad Corazón"),
+("Yakón","Tierra","Ciudad Fayenza","Gimnasio de Ciudad Fayenza"),
+("Gerania","Volador","Ciudad Loza","Gimnasio de Ciudad Loza"),
+("Valeria","Hada","Ciudad Romantis","Gimnasio de Ciudad Romantis"),
+("Ástrid","Psíquico","Ciudad Fluxus","Gimnasio de Ciudad Fluxus"),
+("Mel","Hielo","Pueblo Auriga","Gimnasio de Pueblo Auriga"),
+("Nerio","Siniestro","Pueblo Crampón","Gimnasio de Pueblo Crampón"),
+("Araceli","Bicho","Pueblo Pirotín","Gimnasio de Pueblo Pirotín"),
+("Brais","Planta","Pueblo Altamía","Gimnasio de Pueblo Altamía");
+-- -----------------------------------------------------------------------------------------------------------
+insert into movimiento(Nombre,Tipo,Categoria,Descripcion,PP) values
+("Brock","Roca","Ciudad Plateada","Gimnasio de Ciudad Plateada"),
